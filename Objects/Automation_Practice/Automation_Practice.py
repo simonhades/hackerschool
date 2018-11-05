98faa56b5c9bf93b9208ef03b6c319004961e565
SimonSlp <simon.sloup@seznam.cz>
2018-11-05 19:14:00 +0100

    dd

---
+++ Objects/Automation_Practice/Automation_Practice.py
@@ -0,0 +1,35 @@
+# Main Page
+link_login = "class=login"
+
+# Authentication Page
+auth_tbox_email = "id=email_create"
+btn_create_account = "id=SubmitCreate"
+
+# Sign Up Page
+## Personal Info
+sup_radio_mr = "id=id_gender1"
+sup_radio_mrs = "id=id_gender2"
+sup_tbox_first_name = "id=customer_firstname"
+sup_tbox_last_name = "id=customer_lastname"
+sup_tbox_email = "id=email"
+sup_pwd_password = "id=passwd"
+sup_dropdown_day = "id=days"
+sup_dropdown_month = "id=months"
+sup_dropdown_year = "id=years"
+sup_chck_newsletter = "id=newsletter"
+sup_chck_offers = "id=optin"
+sup_btn_register = "id=submitAccount"
+## Adress
+addr_tbox_first_name = "id=firstname"
+addr_tbox_last_name = "id=lastname"
+addr_tbox_company = "id=company"
+addr_tbox_address1 = "id=address1"
+addr_tbox_address2 = "id=address2"
+addr_tbox_city = "id=city"
+addr_dropdown_state = "id=id_state"
+addr_tbox_zip = "id=postcode"
+addr_dropdown_country = "id=id_country"
+addr_tbox_info = "id=other"
+addr_tbox_phone = "id=phone"
+addr_tbox_cellphone = "id=phone_mobile"
+addr_tbox_alias = "id=alias"
